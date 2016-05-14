09a655493592fe990f2d5977d24b23ef817596c5
jcast27 <castrochavarria27@gmail.com>
2016-05-14 08:50:31 -0600

    proyecto
    iniciando

---
+++ SIME-UTN/SIME-UTN/Entities/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIME_UTN.Entities
+{
+    class Usuario
+    {
+        private static Usuario instancia;
+
+        public string usuario { set; get; }
+        public string contrasenna { set; get; }
+
+        public static Usuario GetInstance()
+        {
+
+            if (instancia == null)
+                instancia = new Usuario();
+
+            return instancia;
+
+        }
+
+    }
+
+}
---
+++ SIME-UTN/SIME-UTN/Gestores/GestorUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIME_UTN.Entities;
+using SIME_UTN.Factories;
+
+namespace SIME_UTN.Gestores
+{
+    sealed class GestorUsuario
+    {
+    public static Usuario usuario;
+    public static string NombreUsuario;
+    public static GestorUsuario instancia;
+
+        public GestorUsuario()
+        {
+            usuario = FactoryUsuario.CrearUsuario();
+        }
+        public static GestorUsuario GetInstance()
+        {
+            if (instancia == null)
+            {
+                instancia = new GestorUsuario();
+            }
+
+            return instancia;
+        }
+        public void InsertarNombreUsuario(string nombre)
+        {
+            NombreUsuario = nombre;
+        }
+        public string OptenerNombreUsuario()
+        {
+            return NombreUsuario;
+        }
+    }
+  
+}
