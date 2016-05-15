407fc8fda2466f507b9411bc81a13ac35da4d4f9
jcast27 <castrochavarria27@gmail.com>
2016-05-15 14:36:30 -0600

    proyecto
    proyecto

---
+++ SIME-UTN/SIME-UTN/Entities/UsuarioDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIME_UTN.Entities
+{
+    class UsuarioDB
+    {
+        private static UsuarioDB instancia;
+
+        public string usuario { set; get; }
+        public string contrasenna { set; get; }
+
+        public static UsuarioDB GetInstance()
+        {
+
+            if (instancia == null)
+                instancia = new UsuarioDB();
+
+            return instancia;
+
+        }
+
+    }
+
+}
