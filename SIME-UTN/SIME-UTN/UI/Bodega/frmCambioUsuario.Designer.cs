a8d66ea8d1712139f14ea2e18402c8c8a48fdc44
elp92 <elopezperez@hotmail.com>
2016-06-07 15:38:04 -0600

    SIME

---
+++ SIME-UTN/SIME-UTN/UI/Bodega/frmCambioUsuario.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace SIME_UTN.UI
+{
+    partial class frmCambioUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasenna = new System.Windows.Forms.TextBox();
+            this.lblSesion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = global::SIME_UTN.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 249);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsuario.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.Color.White;
+            this.lblUsuario.Location = new System.Drawing.Point(320, 90);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(148, 29);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Otro Usuario";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtUsuario.Location = new System.Drawing.Point(325, 203);
+            this.txtUsuario.Multiline = true;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(257, 33);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtContrasenna
+            // 
+            this.txtContrasenna.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtContrasenna.Location = new System.Drawing.Point(325, 276);
+            this.txtContrasenna.Multiline = true;
+            this.txtContrasenna.Name = "txtContrasenna";
+            this.txtContrasenna.PasswordChar = '*';
+            this.txtContrasenna.Size = new System.Drawing.Size(257, 33);
+            this.txtContrasenna.TabIndex = 3;
+            // 
+            // lblSesion
+            // 
+            this.lblSesion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblSesion.AutoSize = true;
+            this.lblSesion.BackColor = System.Drawing.Color.Transparent;
+            this.lblSesion.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSesion.ForeColor = System.Drawing.Color.White;
+            this.lblSesion.Location = new System.Drawing.Point(322, 135);
+            this.lblSesion.Name = "lblSesion";
+            this.lblSesion.Size = new System.Drawing.Size(64, 16);
+            this.lblSesion.TabIndex = 4;
+            this.lblSesion.Text = "En Sesion";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(321, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Contraseña:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(321, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Usuario:";
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnIngresar.Image = global::SIME_UTN.Properties.Resources.Forward_32x32;
+            this.btnIngresar.Location = new System.Drawing.Point(588, 275);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(44, 36);
+            this.btnIngresar.TabIndex = 49;
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmCambioUsuario
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(31)))), ((int)(((byte)(53)))));
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 457);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSesion);
+            this.Controls.Add(this.txtContrasenna);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmCambioUsuario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cambio De Usuario";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrasenna;
+        private System.Windows.Forms.Label lblSesion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ SIME-UTN/SIME-UTN/UI/Bodega/frmMDI.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Threading;
+using DevExpress.XtraBars.Docking2010.Views.WindowsUI;
+using SIME_UTN.Gestores;
+using DevExpress.XtraBars.Docking2010.Views;
+
+namespace SIME_UTN.UI
+{
+    public partial class frmMDI : DevExpress.XtraEditors.XtraForm
+    {
+        static string tipoDeProceso ="";
+        static string usuarioLogueado = "";
+        GestorUsuarioTable gestor = null;
+        public frmMDI()
+        {
+           
+            Thread tardar = new Thread(new ThreadStart(this.SplashScreen));
+            tardar.Start();
+            Thread.Sleep(6000); // Tardanza para iniciar aplicacion (6000)
+            InitializeComponent();
+            tardar.Abort();
+            // Handling the QueryControl event that will populate all automatically generated Documents
+           // CrearDocuemtnos();
+            this.UsuarioLogueado();
+           
+           
+        }
+        public frmMDI(bool estado)
+        {
+
+            InitializeComponent();
+            this.UsuarioLogueado();
+
+        }
+        public void UsuarioLogueado()
+        {
+            gestor = GestorUsuarioTable.GetInstance();
+            usuarioLogueado = gestor.ObtenerUsuarioLogeado();
+            usuarioTSMI.Text = "Usuario: " + usuarioLogueado;
+        }
+
+
+
+        public void SplashScreen()
+        {
+            Application.Run(new frmSplashScreen());
+        }
+
+        // Assigning a required content for each auto generated Document
+        void windowsUIView1_QueryControl(object sender, DevExpress.XtraBars.Docking2010.Views.QueryControlEventArgs e)
+        {
+
+            if (e.Document.Caption == "Ventana Usuarios")
+                e.Control = new SIME_UTN.UI.Administracion.frmUsuarios();
+            if (e.Document.Caption == "Ventana Productos")
+                e.Control = new SIME_UTN.UI.Administracion.frmProductos();
+            if (e.Document.Caption == "Ventana Ingreso Producto")
+                e.Control = new SIME_UTN.UI.Procesos.frmRegistroProducto();
+            //if (e.Control == null)
+            //    e.Control = new System.Windows.Forms.Control();
+
+
+
+        }
+
+        private FlyoutAction createCloseAction(Flyout flyout)
+        {
+            FlyoutAction closeaction = new FlyoutAction() { Caption = "Confirmacion", Description = "Desea Cerrar Esta Aplicacion?" };
+            // closeaction.Description = "Desea Cerrar Esta Aplicacion?";
+            FlyoutCommand comandoYes = new FlyoutCommand() { Text = "Si", Result = System.Windows.Forms.DialogResult.Yes };
+            FlyoutCommand comandoNO = new FlyoutCommand() { Text = "No", Result = System.Windows.Forms.DialogResult.No };
+            closeaction.Commands.Add(comandoYes);
+            closeaction.Commands.Add(comandoNO);
+            return closeaction;
+        }
+
+        private void frmMDI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FlyoutCloseAction.Action = this.createCloseAction(FlyoutCloseAction);
+            if (windowsUIView1.ShowFlyoutDialog(FlyoutCloseAction) != System.Windows.Forms.DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+
+        private void mBtnAdministracion_ElementClick(object sender, DevExpress.XtraBars.Navigation.NavElementEventArgs e)
+        {
+            CrearDocuemtosProceso(false);
+            this.CrearDocuemtosAdministracion(true);
+            tipoDeProceso = "Administracion";
+
+        }
+
+        private void mBtnProcesos_ElementClick(object sender, DevExpress.XtraBars.Navigation.NavElementEventArgs e)
+        {
+            this.CrearDocuemtosAdministracion(false);
+            CrearDocuemtosProceso(true);
+            tipoDeProceso = "Procesos";
+        }
+
+        //Se crea un elemento que sera agregado al frame
+        public TileItemElement CrearElemento(String nombreElemento,Image imagen)
+        {
+            Font font = new Font("Tahoma", 20.0f,FontStyle.Underline| FontStyle.Underline);
+            TileItemElement elemento = new TileItemElement();
+            elemento.Text = nombreElemento;
+            elemento.Image = imagen;
+            elemento.ImageScaleMode = TileItemImageScaleMode.Stretch;
+            elemento.TextAlignment = TileItemContentAlignment.BottomLeft;
+            elemento.Appearance.Normal.ForeColor = Color.Maroon;
+            elemento.Appearance.Normal.BackColor = Color.MediumAquamarine;
+            elemento.Appearance.Normal.Font = font;
+            return elemento;
+        }
+
+        //Se crea el frame que contiene el elemento y q sera agregado a un tile
+        public TileItemFrame crearFrame(String nombreElemento, Image imagen)
+        {
+            TileItemFrame frame = new TileItemFrame();
+            frame.Appearance.BackColor = Color.Transparent;
+            frame.Appearance.BorderColor = Color.Black;
+            frame.Elements.Add(this.CrearElemento(nombreElemento, imagen));
+            return frame;
+        }
+
+        //Se crea un tile que contendra el frame
+        public DevExpress.XtraBars.Docking2010.Views.WindowsUI.Tile crearTile(Document document, String nombreElemento, String grupo, Image imagen,Boolean estado)
+        {
+            DevExpress.XtraBars.Docking2010.Views.WindowsUI.Tile tile1 = new DevExpress.XtraBars.Docking2010.Views.WindowsUI.Tile();
+            tile1.Group = grupo;
+            tile1.Document = document;
+            tile1.Frames.Add(this.crearFrame(nombreElemento, imagen));
+            tile1.Visible = estado;
+            return tile1;
+        }
+
+        public void CrearDocuemtosAdministracion(Boolean estado)
+        {
+            string nombreElemento = "";
+            string grupo = "";
+            Image imagen = null;
+           
+            this.windowsUIView1.BeginUpdate();
+            this.windowsUIView1.Controller.CloseAll();
+            this.windowsUIView1.Documents.Clear();
+            //Creating documents
+            DevExpress.XtraBars.Docking2010.Views.WindowsUI.Document doc1 = new DevExpress.XtraBars.Docking2010.Views.WindowsUI.Document { Caption = "Ventana Usuarios" };
+            DevExpress.XtraBars.Docking2010.Views.WindowsUI.Document doc2 = new DevExpress.XtraBars.Docking2010.Views.WindowsUI.Document { Caption = "Ventana Productos" };
+            this.windowsUIView1.Documents.AddRange(new DevExpress.XtraBars.Docking2010.Views.WindowsUI.Document[] { doc1,doc2 });
+            //Creating and populating content container
+            DevExpress.XtraBars.Docking2010.Views.WindowsUI.TileContainer tileContainer2 = new DevExpress.XtraBars.Docking2010.Views.WindowsUI.TileContainer();
+            tileContainer2.Properties.ItemSize = 200;
+
+            //Propiedades para el decumento Usuarios
+            grupo = "Group 1";
+            imagen = Properties.Resources.usuarios1;
+            nombreElemento = "Usuarios";
+            tileContainer2.Items.Add(this.crearTile(doc1,nombreElemento,grupo,imagen,estado));
+
+            //Propiedades para el decumento Productos
+            grupo = "Group 2";
+            imagen = Properties.Resources.productos1;
+            nombreElemento = "Productos";
+            tileContainer2.Items.Add(this.crearTile(doc2, nombreElemento, grupo, imagen, estado));
+
+            windowsUIView1.ContentContainers.Add(tileContainer2);
+            this.windowsUIView1.ActivateContainer(tileContainer2);
+            this.windowsUIView1.EndUpdate();
+            this.windowsUIView1.QueryControl += windowsUIView1_QueryControl;
+
+        }
+
+        public void CrearDocuemtosProceso(Boolean estado)
+        {
+            string nombreElemento = "";
+            string grupo = "";
+            Image imagen = null;
+
+            this.windowsUIView1.BeginUpdate();
+            this.windowsUIView1.Controller.CloseAll();
+            this.windowsUIView1.Documents.Clear();
+            //Creating documents
+            DevExpress.XtraBars.Docking2010.Views.WindowsUI.Document doc1 = new DevExpress.XtraBars.Docking2010.Views.WindowsUI.Document { Caption = "Ventana Ingreso Producto" };
+            this.windowsUIView1.Documents.AddRange(new DevExpress.XtraBars.Docking2010.Views.WindowsUI.Document[] { doc1});
+            //Creating and populating content container
+            DevExpress.XtraBars.Docking2010.Views.WindowsUI.TileContainer tileContainer2 = new DevExpress.XtraBars.Docking2010.Views.WindowsUI.TileContainer();
+            tileContainer2.Properties.ItemSize = 200;
+
+            //Propiedades para el decumento Ingreso Productos
+            grupo = "Group 1";
+            imagen = Properties.Resources.registroProducto;
+            nombreElemento = "Ingreso De Productos";
+            tileContainer2.Items.Add(this.crearTile(doc1, nombreElemento, grupo, imagen, estado));
+
+
+
+            windowsUIView1.ContentContainers.Add(tileContainer2);
+            this.windowsUIView1.ActivateContainer(tileContainer2);
+            this.windowsUIView1.EndUpdate();
+            this.windowsUIView1.QueryControl += windowsUIView1_QueryControl;
+
+        }
+
+
+        private void windowsUIView1_BackButtonClick(object sender, BackButtonClickEventArgs e)
+        {
+          if (tipoDeProceso == "Administracion")
+            {
+                this.CrearDocuemtosAdministracion(true);
+            }
+            if (tipoDeProceso == "Procesos")
+            {
+                this.CrearDocuemtosProceso(true);
+            }
+             
+        }
+
+        private void windowsUIView1_ControlReleasing(object sender, ControlReleasingEventArgs e)
+        {
+            e.DisposeControl = true;
+        }
+
+
+        private void cambiarSessionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCambioUsuario ofrNuevoUsuario = new frmCambioUsuario(usuarioLogueado);
+            ofrNuevoUsuario.ShowDialog(this);
+
+
+            if (ofrNuevoUsuario.DialogResult == DialogResult.OK)
+            {
+                this.UsuarioLogueado();
+                if (tipoDeProceso == "Administracion")
+                {
+                    this.CrearDocuemtosAdministracion(true);
+                }
+                if (tipoDeProceso == "Procesos")
+                {
+                    this.CrearDocuemtosProceso(true);
+                }
+            }
+
+        }
+
+        private void cambiarContrasennaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCambioContrasenna ofrmCambioContrasenna = new frmCambioContrasenna(usuarioLogueado);
+            ofrmCambioContrasenna.ShowDialog(this);
+        }
+    }
+}
